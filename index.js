e34321a14bbc72ac4d9a4945ff9c2d3cb23b74ba
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-03-21 23:45:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+
+  // The price of each item should be a randomly-generated integer between 1 and 100.
+  var itemPrice = function randomPrice(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var itemName = function randomName (itemName) {
+    return {
+      itemName: itemName,
+      itemPrice: randomPrice(1, 100)
+    }
+  }
+
+  // The object should consist of two key-value pairs : { itemName: name of the item, itemPrice: price of the item, }
+  var item = {itemName: itemName, itemPrice: itemPrice}
+
+  /* As more items are added, the cart should start to look something like this: [ { itemName:"bananas", itemPrice: 17 },
+  { itemName:"pancake batter",itemPrice: 5 }, { itemName:"eggs", itemPrice: 49 }]*/
+  var arr = []
+  for (var i = 0; i < arr.length; i++) {
+   arr.push(`${itemName}: ${price}`)
+  }
+
+  // Upon the successful addition of a new item to the cart, the function should return <itemName> has been added to your cart.
+  getCart().push(item)
+  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
